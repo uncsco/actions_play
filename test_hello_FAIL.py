a661c7a541367c8f908ff1ae6a0f1eff235ad759
uncsco <104643852+uncsco@users.noreply.github.com>
2022-09-26 15:06:46 +0100

    Update test_hello_FAIL.py

--- test_hello_FAIL.py
+++ test_hello_FAIL.py
@@ -1,5 +1,8 @@
 def hello(s: str) -> str:
     return f"Hello, {s}!"
   
-def test_hello():
+def test_hello_fail():
     assert hello("World") == "Hello... World"
+ 
+if __name__ == "__main__":
+    test_hello_fail()  #// Should FAIL
